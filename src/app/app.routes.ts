268e76f733067a16a1472a2e5b361c18a8bc568e
VijayJaganathan <Vijayvivek03@gmail.com>
2024-02-21 10:57:18 +0530

    Route issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { CarDetailsComponent } from './component/car-details/car-details.compone
 import { CarConfigComponent } from './component/car-config/car-config.component';
 
 export const routes: Routes = [
+    { path: '', component: CarModelComponent },
     { path: 'step1', component: CarModelComponent },
     { path: 'step2', component: CarConfigComponent },
     { path: 'step3', component: CarDetailsComponent },
-    { path: '**', redirectTo: 'step1', pathMatch: 'full' }
+    { path: '**', redirectTo: '/step1', pathMatch: 'full' }
 ];
