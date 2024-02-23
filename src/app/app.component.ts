cce066860193759b9ba04df578d08f05244433fc
VijayJaganathan <Vijayvivek03@gmail.com>
2024-02-24 03:44:55 +0530

    Routing issue resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {AsyncPipe, JsonPipe} from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Checkout } from './model/details.model';
 import { ApiService } from './service/api.service';
 
@@ -12,7 +12,9 @@ import { ApiService } from './service/api.service';
 })
 export class AppComponent {
   public checkout!: Checkout;
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private router: Router) { 
+    this.router.navigate(['step1'])
+  }
 
   ngOnInit(): void {
     this.apiService.getCheckoutData().subscribe(checkout => {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,7 @@ import { CarDetailsComponent } from './component/car-details/car-details.compone
 import { CarConfigComponent } from './component/car-config/car-config.component';
 
 export const routes: Routes = [
-    { path: '', component: CarModelComponent },
-    { path: 'step1', component: CarModelComponent },
-    { path: 'step2', component: CarConfigComponent },
-    { path: 'step3', component: CarDetailsComponent },
-    { path: '**', redirectTo: '/step1', pathMatch: 'full' }
+    { path: 'step1', loadComponent: () => import('./component/car-model/car-model.component').then(a => a.CarModelComponent), },
+    { path: 'step2', loadComponent: () => import('./component/car-config/car-config.component').then(a => a.CarConfigComponent), },
+    { path: 'step3', loadComponent: () => import('./component/car-details/car-details.component').then(a => a.CarDetailsComponent), },
 ];
